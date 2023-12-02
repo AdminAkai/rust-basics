7a40e18c2bde99698616dbee3a1de0f1bd1db258
AdminAkai <akatoirotech@gmail.com>
2023-12-02 04:05:40 -0500

    added completed control flow practice

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -1,9 +1,59 @@
 fn main() {
     let one: i32 = 1;
+    if_conditions(one);
+    looping();
+    while_looping();
+    while_looping_with_vector();
+}
 
-    if one > 0 {
-        println!("True")
-    } else {
-        println!("False")
+fn if_conditions(one: i32) {
+        if one > 0 {
+            println!("True")
+        } else if one == one {
+            println!("Equal")
+        } else {
+            println!("False")
+        }
+}
+
+fn looping() {
+    let mut num = 0;
+
+    'counter: loop {
+        println!("Count: {}", num);
+        let mut decrease = 5;
+        loop {
+            println!("Decreasing: {}", decrease);
+            if decrease == 4 {
+                break
+            }
+            if num == 2 {
+                break 'counter
+            }
+            decrease -= 1;
+        }
+        num += 1;
     }
+}
+
+fn while_looping() {
+    let mut num = 0;
+    while num < 5 {
+        println!("Num: {}", num);
+        num += 1;
+    }
+}
+
+fn while_looping_with_vector() {
+    let vec: Vec<i8> = (0..10).collect();
+
+    for element in vec {
+        println!("{}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}", number);
+    }
+    
+    println!("Liftoff!");
 }
